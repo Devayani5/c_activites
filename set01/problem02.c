3f5279efab997568f4459fb62c4b18156d5dc221
Devayani5 <141308808+Devayani5@users.noreply.github.com>
2024-01-24 09:52:45 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -7,7 +7,7 @@ int main()
     scanf("%d",&num1);
     printf("enter the second number:");
     scanf("%d",&num2);
-    int sum= num1+num2;
+    sum= num1+num2;
     printf("the sum of %d and %d is:%d\n",num1,num2,sum);
     return 0;
 }
