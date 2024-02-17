cd4e4b95e3849b891de8db68c2132cfde8799977
Devayani5 <141308808+Devayani5@users.noreply.github.com>
2024-02-17 21:08:59 +0530

    test

--- set01/problem08.c
+++ set01/problem08.c
@@ -1,13 +1,11 @@
-   Write a C program to find sum of _n_ different numbers entered by the user.
+///   Write a C program to find sum of _n_ different numbers entered by the user.
 
-***Function Declarations***
 
-```c
-int input_array_size();
-void input_array(int n, int a[n]);
-int sum_n_array(int n, int a[n]);
-void output(int n, int a[n],int sum);
-```
+//int input_array_size();
+//void input_array(int n, int a[n]);
+//int sum_n_array(int n, int a[n]);
+//void output(int n, int a[n],int sum);
+
 
 #include<stdio.h>
 int main()
