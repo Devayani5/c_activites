986522be889d4fb60575da43fffde20eaa3065a7
Devayani5 <141308808+Devayani5@users.noreply.github.com>
2024-02-19 22:19:18 +0530

    test

--- set01/problem11.c
+++ set01/problem11.c
@@ -1,11 +1,44 @@
- //Write a C program to find the sum of 2 complex numbers
-//struct _complex {
-	//float real;
-	//float imaginary;}
-
-//typedef struct _complex Complex;
-
-//Complex input_complex();
-//Complex add_complex(Complex a, Complex b);
-//void output(Complex a, Complex b, Complex sum);
 #include<stdio.h>
+struct_complex{
+    float real;
+    float img;
+};
+typedef struct_comlex complex;
+{;
+complex input_complex();
+complex add_complex(Complex a,complex b);
+void output(complex a,complex b,complex sum);
+}
+int main()
+{
+    complex a,b,result;
+    printf("For first complex number:\n");
+    a = input_complex();
+    printf("\n For second complex number:\n");
+  b = input_complex();
+  result =add_complex(a,b);
+  output(a,b,result);
+  return 0; 
+}
+complex input_complex()
+{
+    complex x;
+    printf("Enter real part:");
+    scanf("%f",&x.real);
+    printf("enter imaginary part:");
+    scanf("%f",&x.img);
+    return x;
+
+}
+complex add_complex(Complex a,Complex b)
+{
+    complex sum;
+    sum.real = a.real+b.real;
+    sum.img = a.img+b.img;
+    return sum;
+}
+void output(Complex a,complex b,complex sum)
+{
+    printf("The sum of :");
+    printf("%f+%fi and %.f+%.fi is %.f+%.fi\n",a.real,a.img,b.real,b.img);
+}
